63ba7f9eb6841c7605eb8384ca5cbe202a55bd32
odilboooy10 <odilkhon318@gmail.com>
2024-02-14 13:33:32 +0900

    feat: FrontEnd JSni qurish

--- app.js
+++ app.js
@@ -23,12 +23,8 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
    const new_reja = req.body.reja;
    db.collection("plans").insertOne({reja:new_reja}, (err, data) => {
-    if(err) {
-        console.log(err);
-        res.end("Someting went wrong");
-    } else {
-        res.end("Successfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
    });
 });
 
